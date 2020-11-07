9074baed3ce742fcdd968abc869be50571fe5ad0
ronggurmahendra <ronggur.mwp@gmail.com>
2020-11-07 19:04:06 +0700

    bikin komunikasi antara sever dan client dan bikin frontend buat query(untuk result blm

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,9 +1,30 @@
 import time
-from flask import Flask
+from flask import Flask, request
+#import fetch from 'isomorphic-fetch';
 
 app=Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time(), 'id' : [13,14,15]}
-print("hello WOrld !!!")
+#@app.route('/time')
+#def get_current_time():
+#    return {'time': time.time(), 'id' : [13,14,15]}
+
+
+@app.route('/query', methods=['POST'])
+def Post_query():
+    #query = request.form['query']
+    query = request.get_json()
+    #print("query received",query)
+    
+    print("query received")
+    print(query)
+    return "ok"
+
+@app.route('/result')
+def Get_result():
+    print("sending result")
+    return {"content" : [["title1","body1"],["title2","body2"],["title3","body3"]]} #data dummy (sementara matriks string dulu aja), sementara blm dibikin front endnya baru dikirim aja kalau mau liat di console
+    #return {} #diisi yang bakal dikirim ke cllient
+
+if __name__ == '__main__':
+    print("Initializing Server")
+    app.run(debug = True)
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,36 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { Form, Input, Rating, Button } from "semantic-ui-react";
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {n
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.id[1]);
+  const [currentResult, setCurrentResult] = useState(0);
+  //var url = "http://localhost:3000"
+  //const [query,setQuer]
+  useEffect(() => {
+    //console.log("masuk sini")
+    fetch('/result').then(res => res.json()).then(data => {
+      console.log("result received")
+      setCurrentResult(data.content);
       console.log(data);
     });
   }, []);
 
+  function GetQueryFrontEnd(){
+    //a.preventDefault();
+    var query = document.getElementById("query").value;
+    console.log(query);
+    PostQuery(query);
+  }
+
+  function PostQuery(data){
+    console.log("Sending Query")
+    const response = fetch("/query", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <body>            
+        <form>
+          <label>
+            Search : <input type="text" id = "query" name = "query" placeholder = "Search" />
+            <input type="button" value="Search" onClick = {() => GetQueryFrontEnd()} />
+            </label>
+        </form>
+        
+      </body>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-lik"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <p>The current time is {currentTime}.</p>
-      </header>
     </div>
   );
 }
