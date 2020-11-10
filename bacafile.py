39ac600fba2186d273a63e5e5d5729077de9f640
Purecon <mfurqon2001@gmail.com>
2020-11-10 21:23:15 +0700

    Sentence1 and title

--- bacafile.py
+++ bacafile.py
@@ -8,14 +8,24 @@ def getDocumentsFiles():
         arr = os.listdir(path)
         os.chdir(path)
         documents=[]
+        title=[]
+        frstsntc=[]
         #loop untuk setiap file
         for x in arr:
             f=open(x,"r")
             isi = []
+            counter=0
             for i in f:
+                if counter==0:
+                    title.append(i.strip("\n"))
+                    counter=counter+1
+                elif counter==1:
+                    frstsntc.append(i.strip("\n"))
                 isi.append(i.strip("\n"))
             documents.append(' '.join(isi))
         f.close()
-        return documents
-#doc=getDocumentsFiles()        
-#print(doc) 
+        print(title)
+        print(frstsntc) 
+        return (documents,title,frstsntc)
+# doc=getDocumentsFiles()        
+# print(doc)
