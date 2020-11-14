bfecaee5e7d84d2bf58f05ae659fc0ca2880143d
ronggurmahendra <ronggur.mwp@gmail.com>
2020-11-14 18:13:17 +0700

    demo ready version

--- react-flask-app/src/perihal.js
+++ react-flask-app/src/perihal.js
@@ -15,6 +15,15 @@ function Perihal(){
     <div>
         About Us
     </div>
+    <p>
+        Ronggur Mahendra (13519008)
+    </p>
+    <p>
+        Muhammad Furqon (13519184)
+    </p>
+    <p>
+        Ahmad Saladin (13519187)
+    </p>
 
 </div>
     )
